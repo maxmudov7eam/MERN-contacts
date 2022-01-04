32c0407db477228a6ba4cc1d1b29ef5d6f9b7b87
Пользователь <dmaxmudov1609@gmail.com>
2022-01-04 21:32:59 +0300

    'MERN_project_created'

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.listen(5000, () => {
+    console.log('Console log on port 5000.')
+    console.log('Console log on port 5000..')
+    console.log('Console log on port 5000...')
+})
