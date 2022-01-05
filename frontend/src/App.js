22895cb63aece316b3f2d8abc398525360c8036f
Пользователь <dmaxmudov1609@gmail.com>
2022-01-05 19:34:14 +0300

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const [contact, setContact] = useState({
+        name: '',
+        descr: '',
+        number: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setContact(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const { name, descr, number } = contact
+        const newContact = { name, descr, number }
+        console.log(newContact)
+
+    }
+
+
+    return (
+        <>
+            <div className="container mt-5 pt-5">
+                <div className="row">
+                    <div className="col-lg-6 mx-auto">
+                        <form onSubmit={e => e.preventDefault()}>
+                            <input
+                                type="text"
+                                className="form-control mb-4"
+                                placeholder='Enter your name'
+                                name='name'
+                                onChange={handleChange}
+                                value={contact.name}
+                            />
+
+                            <input
+                                type="text"
+                                className="form-control mb-4"
+                                placeholder='Enter description'
+                                name='descr'
+                                onChange={handleChange}
+                                value={contact.descr}
+                            />
+
+                            <input
+                                type="number"
+                                className="form-control mb-4"
+                                placeholder='Enter your number'
+                                name='number'
+                                onChange={handleChange}
+                                value={contact.number}
+                            />
+
+                            <button
+                                className="btn btn-outline-dark d-block ml-auto"
+                                onClick={submitHandler}
+                            >
+                                Send me
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
